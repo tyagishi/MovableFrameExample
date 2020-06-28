ebedb5608aed4560f93f190ab86bd39f11d852e6
Tomoaki Yagishita <tyagishi@gmail.com>
2020-06-28 21:02:55 +0900

    Initial Commit

---
+++ MovableFrameExample/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  MovableFrameExample
+//
+//  Created by Tomoaki Yagishita on 2020/06/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
