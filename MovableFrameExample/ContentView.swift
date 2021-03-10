fa9d1d2196486c7bf87eba25e4c638b19a7608a6
Tomoaki Yagishita <tyagishi@gmail.com>
2021-03-10 17:07:11 +0900

    align to imageRect with shift-key

--- MovableFrameExample/ContentView.swift
+++ MovableFrameExample/ContentView.swift
@@ -7,21 +7,27 @@
 
 import SwiftUI
 import MovableFrame
+import SDSCGExtension
+import SwiftUIDebugUtil
 
 struct ContentView: View {
     @State private var frameRect: CGRect = CGRect(origin: CGPoint.zero,
                                                   size: CGSize(width: 300, height: 200))
     @State private var canvasRect: CGRect = CGRect.zero
     
-    @State private var nsImage: NSImage? = nil
+    @State private var nsImage: NSImage = NSImage(named: "SampleImage")!
 
     var body: some View {
         ZStack {
-            Image("SampleImage")
+            Image(nsImage: nsImage)
+                .resizable()
+                .scaledToFit()
                 .frame(maxWidth: .infinity, maxHeight: .infinity)
                 .background(FrameViewRectPreferenceSetter(prefName: "ImageView"))
-            FrameView(frameRect: $frameRect, canvasRect: canvasRect)
+                .debugBorder(Color.yellow, width: 3)
+            FrameView(frameRect: $frameRect, canvasRect: canvasRect, imageRect: nsImage.size.placeInSizedCanvas(canvasRect.size))
         }
+        .debugBorder(.red, width: 4)
         .onPreferenceChange(FrameViewRectPreferenceKey.self, perform: { prefs in
             for pref in prefs {
                 if pref.name == "ImageView" {
